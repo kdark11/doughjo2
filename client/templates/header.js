8946638d7511a2b12f4f2fbc93c44e111f1a7c7c
kdark11 <k.dark11@gmail.com>
2014-11-24 23:05:17 -0700

    added some client folders

--- client/templates/header.js
+++ client/templates/header.js
@@ -1,3 +1,14 @@
 // first, we define a default state
 // client/templates/header.js 
 Session.setDefault('active_page', 'home');
+
+// then we need to add some event triggers
+// client/views/app.navbars.js 
+Template.navbarHeader.events({
+  'click #homePageButton': function(){
+    Session.set('active_page', 'main');
+  },
+  'click #aboutPageButton': function(){
+    Session.set('active_page', 'about');
+  }
+});
